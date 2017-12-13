7dcc1d3d28638a4cd8f024b8e52ba2e0ae1b2a35
1wsx10 <1wsx10@gmail.com>
2017-12-13 21:11:57 +1100

    added prototype

---
+++ prototype.cs
@@ -0,0 +1,11 @@
+public Program() {}
+
+public void Main() {
+	IMyParachute chute = GridTerminalSystem.GetBlockWithName("chute");
+	IMyShipController cont = GridTerminalSystem.GetBlockWithName("controller");
+	IMyGyro gyro = GridTerminalSystem.GetBlockWithName("gyro");
+
+
+}
+
+public void Save() {}
